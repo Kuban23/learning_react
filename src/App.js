c02484ccba397135002857f53faaf02aa0cabb24
Esin Andrew <kubanesin@gmail.com>
2022-12-15 20:44:45 +0300

    Реализовал стили и создал будущую страницу

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './styles/App.css';
+
+
+function App() {
+
+
+
+   return (
+
+      <div className='App'>
+
+         App
+
+      </div>
+
+   );
+}
+
+export default App;
