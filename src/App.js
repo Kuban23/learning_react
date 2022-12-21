c0999d279e3e9a27f246dc7f76dd431e88b89e73
Esin Andrew <kubanesin@gmail.com>
2022-12-21 20:23:22 +0300

    реализовал поиск постов

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import PostForm from './Components/PostForm/PostForm';
 // import Counter from './Components/Counter/Counter';
 
 import PostList from './Components/PostList/PostList';
+import MyInput from './Components/UI/Input/MyInput';
 import MySelect from './Components/UI/MySelect/MySelect';
 // import Mybutton from './Components/UI/Button/Mybutton';
 // import MyInput from './Components/UI/Input/MyInput';
@@ -23,7 +24,23 @@ function App() {
    ]);
 
    // Состояние селекта
-   const [selectedSort, setSelectedSort]= React.useState('');
+   const [selectedSort, setSelectedSort] = React.useState('');
+
+   // Состояние инпута поиска
+   const [search, setSearch] = React.useState('');
+
+   // Хук для отслеживания изменения постов и состояния сортировки, возвращаю массив отсортированных постов
+   const sortedPosts = React.useMemo(() => {
+      if (selectedSort) {
+         return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+      }
+      return posts
+   }, [posts, selectedSort]);
+
+   // Хук для отслеживания изменения инпута поиска и отсортированного массива постов
+   const sortedAndSearchedPosts =React.useMemo(()=>{
+    return sortedPosts.filter((post)=>post.title.toLowerCase().includes(search))
+   },[sortedPosts, search]);
 
    // Функция добовления постов, ее прокидываю пропсами в PostForm
    const createPost = (Newpost) => {
@@ -31,13 +48,13 @@ function App() {
    }
 
    // Функция для удаления постов, ее прокидываю пропсами в PostItem через PostList
-   const removePost= (post) => {   
-     setPosts(posts.filter((p)=> p.id !== post.id))
+   const removePost = (post) => {
+      setPosts(posts.filter((p) => p.id !== post.id))
    }
 
    // Функция сортировки постов
    const sortedPost = (sortPost) => {
-      setPosts([...posts].sort((a,b)=>a[sortPost].localeCompare(b[sortPost])));
+      // setPosts([...posts].sort((a, b) => a[sortPost].localeCompare(b[sortPost])));
       setSelectedSort(sortPost)
    }
 
@@ -47,24 +64,30 @@ function App() {
 
          <PostForm
             create={createPost}
-         />         
+         />
+
+         <MyInput
+            placeholder={'Поиск...'}
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+         />
 
          <MySelect
-         defaultValue={'Сортировка по...'}
-         options = {[
-            {value: 'title', name: 'По наименованию' },
-            {value: 'body', name: 'По описанию' }
-         ]}
-         value={selectedSort}
-         onChange={sortedPost}
-         
+            defaultValue={'Сортировка по...'}
+            options={[
+               { value: 'title', name: 'По наименованию' },
+               { value: 'body', name: 'По описанию' }
+            ]}
+            value={selectedSort}
+            onChange={sortedPost}
+
          />
-        
+
          {
-            posts.length !== 0
-             ?    <PostList posts={posts} remove={removePost}/>
-             : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
-         }       
+            sortedAndSearchedPosts.length !== 0
+               ? <PostList posts={sortedAndSearchedPosts} remove={removePost} />
+               : <h1 style={{ textAlign: 'center' }}>Посты не найдены</h1>
+         }
 
       </div>
 
