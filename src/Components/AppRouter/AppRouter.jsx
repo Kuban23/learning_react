a93cfe5b1c4f0ee2f7aedd64ca3534a0e23871f8
Esin Andrew <kubanesin@gmail.com>
2023-01-18 17:24:32 +0300

    оптимизировал роуты, вынес в отдельный файл и массив

--- src/Components/AppRouter/AppRouter.jsx
+++ src/Components/AppRouter/AppRouter.jsx
@@ -3,17 +3,30 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import About from '../../Components/Pages/About';
 import NotFoundPage from '../../Components/Pages/NotFoundPage';
 import Posts from '../../Components/Pages/Posts';
+import { routers } from '../../hook/router/routers';
 import PostIdPage from '../Pages/PostIdPage';
 
 const AppRouter = () => {
    return (
       <>
          <Routes>
-            <Route path='/about' element={<About />} />
+
+            {routers.map((rout) =>
+               <Route
+                  path={rout.path}
+                  element={rout.element}
+                  exact={rout.exact}
+                  key={rout.path}
+               />
+            )
+            }
+
+
+            {/* <Route path='/about' element={<About />} />
             <Route exact path='/posts' element={<Posts />} />
-            <Route exact path='/posts/:id' element={<PostIdPage />} />
+            <Route exact path='/posts/:id' element={<PostIdPage />} /> */}
             {/* <Route path='*' element={<NotFoundPage />} /> */}
-            <Route path='*' element={<Navigate to='/posts' replace />}/>
+            <Route path='*' element={<Navigate to='/posts' replace />} />
          </Routes>
 
       </>
--- src/hook/router/routers.js
+++ src/hook/router/routers.js
@@ -1,3 +1,9 @@
+import About from "../../Components/Pages/About";
+import PostIdPage from "../../Components/Pages/PostIdPage";
+import Posts from "../../Components/Pages/Posts";
+
 export const routers = [
-   { path:},
+   { path: '/about', element: <About />, exact: true },
+   { path: '/posts', element: <Posts />, exact: true },
+   { path: '/posts/:id', element: <PostIdPage />, exact: true },
 ]; 
