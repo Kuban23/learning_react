d39dd4a3d891f022a2d09a34e5f1d5035df3b968
Esin Andrew <kubanesin@gmail.com>
2023-01-19 15:19:44 +0300

    авторизация, сохранение пользователя в localStorage

--- src/Components/UI/Navbar/Navbar.jsx
+++ src/Components/UI/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { Link} from 'react-router-dom';
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../Context';
+import MyButton from '../Button/MyButton';
 
 const Navbar = () => {
-  return (
-   <div className='navbar'>
-   <Link className='navbar__links' to='/about'>О проекте</Link>
-   <Link className='navbar__links' to='/posts'>Посты</Link>
-</div>
-  )
+
+   const { isAuth, setIsAuth } = useContext(AuthContext);
+
+   //Функция для выхода из приложения
+   const logout = () => {
+      setIsAuth(false)
+      localStorage.removeItem('auth')
+   }
+
+   return (
+      <div className='navbar'>
+         <Link className='navbar__links' to='/about'>О проекте</Link>
+         <Link className='navbar__links' to='/posts'>Посты</Link>
+         <button className='navbar__links navbar__btn' onClick={logout}>Выход</button>
+      </div>
+   )
 }
 
 export default Navbar
